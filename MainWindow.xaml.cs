b4c2013083d1d51a120ae280fe64dfd9d7a8db73
aplesa <aplesa@sdl.com>
2021-11-04 11:10:22 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Switch_Trados_Studios_Environment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        private readonly OverwriteStudioEnvironment _overwriteStudioEnvironment = new OverwriteStudioEnvironment();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _overwriteStudioEnvironment.PopulateEnvironmentDictionaries();
+            _overwriteStudioEnvironment.PopulateStudioDictionaries();
+
+            var availableStudioBuilds = _overwriteStudioEnvironment.GetListOfInstalledTradosStudioBuilds();
+            
+            foreach (string build in availableStudioBuilds)
+            {
+                ComboBoxItem comboBoxItem = new ComboBoxItem();
+                comboBoxItem.Background = Brushes.White;
+                comboBoxItem.Content = build;
+                StudioBuildType.Items.Add(comboBoxItem);
+            }
+            if (availableStudioBuilds.Length == 1)
+            {
+                StudioBuildType.SelectedIndex = 0;
+            }
+            else if (availableStudioBuilds.Length == 0)
+            {
+                const string message = "There isn't any Trados Studio Build installed\n\nPlease Install one and restart the tool or add a custom path";
+                Status.Visibility = Visibility.Visible;
+                Status.Text = message;
+                Status.Foreground = Brushes.DarkRed;
+                SwitchEnvironmentButton.IsEnabled = false;
+            }
+
+            string environmentFolderPath = _overwriteStudioEnvironment.GetEnvironmentFolder();
+            var environmentConfigFiles = Directory.GetFiles(environmentFolderPath);
+
+            foreach (string environmentFile in environmentConfigFiles)
+            {
+                string fileName = new FileInfo(environmentFile).Name;
+                ComboBoxItem comboBoxItem = new ComboBoxItem();
+                comboBoxItem.Background = Brushes.White;
+                comboBoxItem.Content = System.IO.Path.GetFileNameWithoutExtension(fileName);
+                EnvironmentType.Items.Add(comboBoxItem);
+            }
+        }
+
+        private void ChangeEnvironment_Click(object sender, RoutedEventArgs e)
+        {
+            var studioIndex = StudioBuildType.SelectedIndex;
+            var environmentIndex = EnvironmentType.SelectedIndex;
+            var studioProcesses = _overwriteStudioEnvironment.GetStudioProcesses();
+
+            if (studioIndex == -1 || environmentIndex == -1)
+            {
+                const string message = "Select which Trados Studio build you want to change \n\nAnd what environment type you want to change to";
+                Status.Visibility = Visibility.Visible;
+                Status.Text = message;
+                Status.Foreground = Brushes.Black;
+                return;
+            }
+
+            if (studioProcesses.Length != 0)
+            {
+                const string message = "Trados Studio needs to be closed before changing the environment. \n\nDo you want to close it?";
+                const string caption = "Close Studio Confirmation";
+                var result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.No)
+                {
+                    return;
+                }
+                _overwriteStudioEnvironment.CloseStudio(studioProcesses);
+            }
+
+            _overwriteStudioEnvironment.DeleteLanguageCloudLoggedInCofig(studioIndex);
+
+            SwitchEnvironmentButton.IsEnabled = false;
+            try
+            {
+                string message = string.Format("Success:\n\n{0} has been changed to the {1}", StudioBuildType.Text, EnvironmentType.Text);
+                _overwriteStudioEnvironment.SwitchStudiosLcEnvironment(studioIndex, environmentIndex);
+                Status.Visibility = Visibility.Visible;
+                Status.Text = message;
+                Status.Foreground = Brushes.Green;
+            }
+            catch(Exception exception)
+            {
+                Status.Visibility = Visibility.Visible;
+                Status.Text = String.Format("Failed:\n\n{0}", exception.Message);
+                Status.Foreground = Brushes.OrangeRed;
+            }
+            SwitchEnvironmentButton.IsEnabled = true;
+        }
+
+        private void CustomLocation_Click(object sender, RoutedEventArgs e)
+        {
+            string customLocation = _overwriteStudioEnvironment.ChooseCustomLocation();
+            if (customLocation != string.Empty)
+            {
+                bool customPathAdded = _overwriteStudioEnvironment.AddCustomBuildPathToDictionary(customLocation);
+                if (customPathAdded)
+                {
+                    StudioBuildType.Items.Add(customLocation);
+                }
+                StudioBuildType.SelectedItem = customLocation;
+                SwitchEnvironmentButton.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ OverwriteStudioEnvironment.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using Microsoft.Win32;
+using System.Linq;
+
+namespace Switch_Trados_Studios_Environment
+{
+    class OverwriteStudioEnvironment
+    {
+        Dictionary<int, string> studioBuildTypeDictionary = new Dictionary<int, string>();
+        Dictionary<int, string> environmentDictionary = new Dictionary<int, string>();
+        private int nrOfStudioBuilds;
+
+        private static string _machineUser = Environment.UserName;
+        private const string LanguageCloudMachineTranslation = "LanguageCloudMachineTranslation.bin";
+        private const string BestMatchServiceSettingsNode = "/configuration/BestMatchServiceSettings";
+        private const string BestMatchServiceUrlsConfigNode = "/configuration/BestMatchServiceUrlsConfig";
+        private const string SdlInstallRegistryPath = @"SOFTWARE\WOW6432Node\SDL";
+
+        public string[] GetListOfInstalledTradosStudioBuilds()
+        {
+            RegistryKey SdlInstallRegistry = Registry.LocalMachine.OpenSubKey(SdlInstallRegistryPath);
+            if (SdlInstallRegistry != null)
+            {
+                var installedStudioBuilds = SdlInstallRegistry.GetSubKeyNames()
+                .Where<string>(val => (val.Contains("Studio16") || val.Contains("Studio15")) && !val.Contains("License")).ToArray();
+                return installedStudioBuilds;
+            }
+            return new string[0];
+        }
+
+        public string ChooseCustomLocation()
+        {
+            var dialog = new OpenFileDialog();
+            try
+            {
+                dialog.ShowDialog();
+                return dialog.FileName;
+            }
+            catch 
+            {
+                return string.Empty;
+            }
+        }
+
+        public void SwitchStudiosLcEnvironment(int selectedStudioTypIndexe, int environment)
+        {
+            XmlDocument bestMatchServicesSettings = new XmlDocument();
+            bestMatchServicesSettings.Load(GetPathToTheSpecificEnvironmentFile(environment));
+            var newBestMatchServiceSettings = bestMatchServicesSettings.SelectSingleNode(BestMatchServiceSettingsNode);
+            var newBestMatchServiceUrlConfig = bestMatchServicesSettings.SelectSingleNode(BestMatchServiceUrlsConfigNode);
+
+            XmlDocument tradosStudioConfigFile = new XmlDocument();
+            string lcEnvironmentFilePath = nrOfStudioBuilds <= selectedStudioTypIndexe ? 
+                studioBuildTypeDictionary[selectedStudioTypIndexe] : 
+                GetInstallPathToTheSpecificStudioBuild(selectedStudioTypIndexe);
+            
+            tradosStudioConfigFile.Load(lcEnvironmentFilePath);
+            var oldBestMatchServiceSettings = tradosStudioConfigFile.SelectSingleNode(BestMatchServiceSettingsNode);
+            var oldBestMatchServiceUrlConfig = tradosStudioConfigFile.SelectSingleNode(BestMatchServiceUrlsConfigNode);
+
+            var configuration = oldBestMatchServiceSettings.ParentNode;
+
+            configuration.InsertBefore(tradosStudioConfigFile.ImportNode(newBestMatchServiceSettings, true), oldBestMatchServiceSettings);
+            configuration.RemoveChild(oldBestMatchServiceSettings);
+
+            configuration.InsertBefore(tradosStudioConfigFile.ImportNode(newBestMatchServiceUrlConfig, true), oldBestMatchServiceUrlConfig);
+            configuration.RemoveChild(oldBestMatchServiceUrlConfig);
+
+            tradosStudioConfigFile.Save(lcEnvironmentFilePath);
+        }
+
+        public string GetEnvironmentFolder()
+        {
+            string environmentFilesLocation = Path.Combine(Environment.CurrentDirectory, @"Environment Files\");
+            return environmentFilesLocation;
+        }
+
+        public string GetPathToTheSpecificEnvironmentFile(int environment)
+        {
+            string environmentFolderPath = GetEnvironmentFolder();
+            return Path.Combine(environmentFolderPath, environmentDictionary[environment]);
+        }
+
+        public string GetInstallPathToTheSpecificStudioBuild(int studioBuildType)
+        {
+            string buildRegistryKeyPath = Path.Combine(SdlInstallRegistryPath, studioBuildTypeDictionary[studioBuildType]);
+            RegistryKey SdlInstallRegistry = Registry.LocalMachine.OpenSubKey(buildRegistryKeyPath);
+            var installLocation = SdlInstallRegistry.GetValue("InstallLocation");
+            return (string)installLocation;
+        }
+
+        public void PopulateStudioDictionaries()
+        {
+            var installedStudioBuilds = GetListOfInstalledTradosStudioBuilds();
+            int Key = 0;
+            foreach(string build in installedStudioBuilds)
+            {
+                studioBuildTypeDictionary.Add(Key, build);
+                Key++;
+            }
+            nrOfStudioBuilds = studioBuildTypeDictionary.Count;
+        }
+
+        public bool AddCustomBuildPathToDictionary(string customPath)
+        {
+            bool valueExists = studioBuildTypeDictionary.Values.Contains<string>(customPath);
+            if (!valueExists)
+            {
+                int Key = studioBuildTypeDictionary.Count;
+                studioBuildTypeDictionary.Add(Key, customPath);
+                return true;
+            }
+            return false;
+        }
+
+        public void PopulateEnvironmentDictionaries()
+        {
+            string environmentFolderPath = GetEnvironmentFolder();
+            var environmentConfigFiles = Directory.GetFiles(environmentFolderPath);
+
+            for (int position = 0; position < environmentConfigFiles.Count(); position++)
+            {
+                FileInfo fileInfo = new FileInfo(environmentConfigFiles[position]);
+                environmentDictionary.Add(position, fileInfo.Name);
+            }
+        }
+
+        public void CloseStudio(Process[] studioProcesses)
+        {
+            foreach (Process proc in studioProcesses)
+            {
+                proc.Kill();
+            }
+        }
+
+        public Process[] GetStudioProcesses()
+        {
+            return Process.GetProcessesByName("SDLTradosStudio");
+        }
+
+        public void DeleteLanguageCloudLoggedInCofig(int studioBuildType)
+        {
+            string buildType = nrOfStudioBuilds <= studioBuildType ? "Studio16" : studioBuildTypeDictionary[studioBuildType];
+            string loginFileLocation = string.Format(@"C:\Users\{0}\AppData\Roaming\SDL\SDL Trados Studio\{1}\{2}", _machineUser, buildType, LanguageCloudMachineTranslation);
+            try
+            {
+                FileInfo fileInfo = new FileInfo(loginFileLocation);
+                fileInfo.Delete();
+            }
+#pragma warning disable RCS1075 // Avoid empty catch clause that catches System.Exception.
+            catch (Exception) { }
+#pragma warning restore RCS1075 // Avoid empty catch clause that catches System.Exception.
+        }
+    }
+}
