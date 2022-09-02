187c5b7b6c5336223b279e6daabc68f4b449ed81
Alexandru Plesa <aplesa@sdl.com>
2022-09-02 15:59:22 +0300

    Fixed the delete LC login file when the user folder from appdata also has the Domain next to it
    Updated the tool to use .Net6

--- LanguageCloudConfig.cs
+++ LanguageCloudConfig.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Switch_Trados_Studios_Environment
 {
     internal class LanguageCloudConfig
     {
 
-        private static string _machineUser = Environment.UserName;
+        private static string _machineUserPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         InstalledBuilds installedBuilds = new InstalledBuilds();
 
         public void DeleteLanguageCloudConfig(int studioBuildType)
@@ -24,11 +20,11 @@ namespace Switch_Trados_Studios_Environment
             {
                 buildType = "Studio17";
             }
-            string languageCloudMachineTranslationLocation = buildType.Contains("17") ? @"Trados\Trados Studio" : @"SDL\SDL Trados Studio";
-            
+            string languageCloudMachineTranslationLocation = buildType.Contains("17") ? "Trados\\Trados Studio" : "SDL\\SDL Trados Studio";
+
             try
             {
-                string loginFileLocation = $@"C:\Users\{_machineUser}\AppData\Roaming\{languageCloudMachineTranslationLocation}\{buildType}\{Constants.LanguageCloudMachineTranslation}";
+                string loginFileLocation = $"{_machineUserPath}\\AppData\\Roaming\\{languageCloudMachineTranslationLocation}\\{buildType}\\{Constants.LanguageCloudMachineTranslation}";
                 FileInfo fileInfo = new FileInfo(loginFileLocation);
                 fileInfo.Delete();
             }
